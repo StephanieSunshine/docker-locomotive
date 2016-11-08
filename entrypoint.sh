b964c5c6ce10d3002f148b3d1274e572a6b33106
Stephanie Sunshine <StephanieSunshine@users.noreply.github.com>
2016-11-07 22:59:03 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,38 +1,19 @@
 #!/usr/bin/env bash
 source /etc/profile.d/rvm.sh
 
-echo
-echo "Setting system timezone..."
-ln -s /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
-dpkg-reconfigure -f noninteractive tzdata
-
-#echo "America/Los_Angeles" > /etc/timezone
-#dpkg-reconfigure -f noninteractive tzdata
-
-echo
-echo "Setting GEM environment..."
-
-rm -rf ~root/.gem
-#export GEM_HOME /var/lib/gems/2.2.0
-#export GEM_PATH /var/lib/gems/2.2.0
-#export GEM_SPEC_CACHE /var/lib/gems/2.2.0/specs
-gem environment
-
-echo
 echo "Installing latest bundler..."
-gem uninstall bundler-1.10.6
 gem install bundler
 
 if [ ! -f /home/app/engine/app ]; then
 	echo
 	echo "Setting up Ruby on Rails..."
 	gem install rails -v 4.2.7.1
-	#ln -sf /var/lib/gems/2.2.0/gems/railties-4.2.6/bin/rails /usr/local/bin/rails
 	cd /home/app; rails new engine --skip-bundle --skip-active-record --skip
 	cd /home/app/engine
 	
 	echo "gem 'locomotivecms', '~> 3.1.1', :git => 'https://github.com/locomotivecms/engine.git', :tag => 'v3.1.1'" >> "Gemfile"
 	echo "gem 'puma'" >> "Gemfile"
+	echo "gem 'therubyracer', platforms: :ruby" >> "Gemfile"
 fi
 
 echo 
@@ -45,13 +26,14 @@ if [ ! -f config/initializers/locomotive.rb ]; then
 	echo "Installing locomotive..."
 	cd /home/app/engine; rails generate locomotive:install
 	sed -i 's/localhost/db/g' /home/app/engine/config/mongoid.yml
+	rake secret > SECRET_KEY_BASE
 fi
 chown app:app -R /home/app
 
 echo
 echo "Compiling Assets..."
-su - app -c "cd /home/app/engine; RAILS_ENV=production bundle exec rake assets:precompile --trace"
+su - app -c "cd /home/app/engine; SECRET_KEY_BASE=`cat SECRET_KEY_BASE` RAILS_ENV=production bundle exec rake assets:precompile --trace"
 
 echo
 echo "Starting Phusion Passenger Stand-alone..."
-RAILS_ENV=production bundle exec passenger start -a 0.0.0.0 -p 80 -e production
+SECRET_KEY_BASE=`cat SECRET_KEY_BASE` RAILS_ENV=production bundle exec passenger start -a 0.0.0.0 -p 80 -e production
